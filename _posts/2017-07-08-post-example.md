3bb5de7a53854d95894f43d565d0cbd2cbe1bf27
Giampiero <giampiero7@users.noreply.github.com>
2017-07-16 16:54:58 +0200

    DD

---
+++ _posts/2017-07-08-post-example.md
@@ -0,0 +1,16 @@
+---
+layout: post
+
+title:  "Test Post 1"
+subtitle: "aiosdj askldjaios dioasjdioa sidja"
+categories: dumpster-diving pathological-empathy
+tags: blu red bla
+img-small: test-post-small.png
+img-large: test-post-large.png
+
+ready: false
+---
+
+This part is what is shown in the excerpt
+<!--more-->
+Here is all the rest...
--- js/custom.js
+++ js/custom.js
@@ -1,9 +1,3 @@
-/*!
- * Start Bootstrap - Grayscale Bootstrap Theme (http://startbootstrap.com)
- * Code licensed under the Apache License v2.0.
- * For details, see http://www.apache.org/licenses/LICENSE-2.0.
- */
-
 // jQuery to collapse the navbar on scroll
 $(window).scroll(function() {
     if ($(".navbar").offset().top > 50) {
@@ -15,22 +9,11 @@ $(window).scroll(function() {
 
 // jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function() {
-    $('a.page-scroll').bind('click', function(event) {
+    $('.page-scroll').bind('click', function(event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
+            scrollTop: $($anchor.attr('scroll-to')).offset().top - 120
         }, 1500, 'easeInOutExpo');
         event.preventDefault();
     });
 });
-
-// Closes the Responsive Menu on Menu Item Click
-$('.navbar-collapse ul li a').click(function() {
-    $('.navbar-toggle:visible').click();
-});
-
-// remove the focused state after click,
-// otherwise bootstrap will still highlight the link
-$("a").mouseup(function(){
-    $(this).blur();
-})
