40b02e2f150fa6da07ac95765eec948faac1cf38
Giampiero <giampiero7@users.noreply.github.com>
2017-09-30 16:29:27 +0200

    cookie consent

--- js/custom.js
+++ js/custom.js
@@ -17,3 +17,16 @@ $(function() {
         event.preventDefault();
     });
 });
+
+// cookie policy
+window.addEventListener("load", function(){
+  window.cookieconsent.initialise({
+    "palette": {
+      "popup": {
+        "background": "#000"
+      },
+      "button": {
+        "background": "#f1d600"
+      }
+    }
+})});
